4f0ba7346df2fb0f5033358a4f1fe475926a3d32
Erik <erikhau3010@gmail.com>
2021-10-09 21:33:35 +0700

    chore: fix vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "memory-game-card" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/memory-game-card/" : "/",
 };
